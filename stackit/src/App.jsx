61a0f665bc5ed7d00b6f6cc19dd0d34bf10f5214
Krish_2005 <me.coder.in@gmail.com>
2025-07-12 17:18:16 +0530

    Update project title and integrate @n8n/chat for enhanced chat functionality

--- stackit/src/App.jsx
+++ stackit/src/App.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Layouts/Home";
 import Question from "./Layouts/Question";
@@ -8,6 +11,12 @@ import { AuthProvider } from "./contexts/AuthContext"; // <-- import
 import SignUp from "./Layouts/SignUp";
 
 export default function App() {
+  useEffect(() => {
+		createChat({
+			webhookUrl: 'http://localhost:5678/webhook/624c7637-20b4-46f7-884c-40fc6b7f7a3f/chat'
+		});
+	}, []);
+
   return (
     <AuthProvider>
       <Router>
